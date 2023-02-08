b075f95108c461c85253fbf8d7f78c8e02f58521
João <joaomanoeldev@gmail.com>
2023-02-08 16:44:24 -0300

    Finishing application testing

--- src/database.ts
+++ src/database.ts
@@ -2,8 +2,6 @@ import 'dotenv/config'
 import { knex as setupKnex, Knex } from 'knex'
 import { env } from './env'
 
-console.log(process.env)
-
 if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL env not found.')
 }
